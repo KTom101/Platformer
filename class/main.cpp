cc8efdf14a4d4b5e4da4bae77f2b5f6c263e46cd
Divya Madala <divyamadala@10-18-211-45.dynapool.wireless.nyu.edu>
2019-10-28 11:19:14 -0400

    Initial Commit

---
+++ class/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  class
+//
+//  Created by Divya Madala on 10/28/19.
+//  Copyright © 2019 dm3980. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
